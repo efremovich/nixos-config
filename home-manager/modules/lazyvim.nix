b8f0adc780db8594468d332b3c0dfcadefed3433
Efremovich <sasha.fima@gmail.com>
2025-08-12 00:04:59 +0300

    home soft

--- home-manager/modules/lazyvim.nix
+++ home-manager/modules/lazyvim.nix
@@ -1,5 +1,22 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   imports = [ inputs.LazyVim.homeManagerModules.default ];
 
-  programs.lazyvim = { enable = true; };
+  programs.lazyvim = { enable = true;
+ # Add LSP servers and tools
+  extraPackages = with pkgs; [
+    rust-analyzer
+    gopls
+    nil
+    nixfmt
+    nodePackages.typescript-language-server
+  ];
+  
+  # Add treesitter parsers
+  treesitterParsers = with pkgs.tree-sitter-grammars; [
+    tree-sitter-rust
+    tree-sitter-go
+    tree-sitter-typescript
+    tree-sitter-tsx
+  ];
+ };
 }
