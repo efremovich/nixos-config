edbde2137a0f13903d1f810fffcdfc808c307e5f
Efremov Aleksandr <efremov_an@astralnalog.ru>
2025-08-11 19:40:58 +0300

    update xsh

--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -1,11 +1,14 @@
 { pkgs, user, ... }: {
   programs.fish.enable = true;
+  programs.zsh.enable = true;
 
   users = {
-    defaultUserShell = pkgs.fish;
+    defaultUserShell = pkgs.zsh;
     users.${user} = {
       isNormalUser = true;
       extraGroups = [ "wheel" "networkmanager" ];
     };
   };
+
+  services.getty.autologinUser = user;
 }
